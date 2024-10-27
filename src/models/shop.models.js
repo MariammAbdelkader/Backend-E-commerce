81b01d8dea37ac5a7cf6c875a01d340773d8598a
Ahmed Khamis <ahmeddkhamiess10@gmail.com>
2024-10-27 23:44:24 +0300

    fix errors
    Signed-off-by: Ahmed Khamis <ahmeddkhamiess10@gmail.com>

--- src/models/shop.models.js
+++ src/models/shop.models.js
@@ -1,1 +1,80 @@
-// TO DO : shop models and its relations
+const {  DataTypes } = require('sequelize');
+const { db } = require('../database');
+
+const Shop = db.define('Shop', {
+    shopId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    metaData: {
+        type: DataTypes.JSON,
+        allowNull: true,
+    },
+    domain: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+});
+
+
+const Product = db.define('Product', {
+    productId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    shopId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Shops',
+            key: 'shopId',
+        },
+    },
+});
+
+// Image Model
+const ProductImage = db.define('ProductImages', {
+    imageId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    altText: {
+        type: DataTypes.STRING,
+        allowNull: true, // Optional image description
+    },
+    productId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Products',
+            key: 'productId',
+        },
+    },
+  });
+
+
+
+module.exports={Shop,Product,ProductImage}
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,1 +1,48 @@
-// TO DO : user models and its relations
+
+const { DataTypes } = require('sequelize');
+const { db } = require('../database');
+
+const ShopOwner = db.define('ShopOwner', {
+    ownerId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: {
+                args: [8, 15],
+                msg: 'Password must be between 8 and 15 characters long.',
+            },
+            is: {
+                args: /^(?=.*[A-Z]).*$/,
+                msg: 'Password must contain at least one uppercase letter.',
+            },
+        },
+    },
+    shopId: {  
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'Shops',
+            key: 'shopId',
+        },
+    },
+});
+
+
+
+module.exports = { ShopOwner}
