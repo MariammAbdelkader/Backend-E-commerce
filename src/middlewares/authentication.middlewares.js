273d070be42739f2c6e4025343800f2aaa0c6141
YoussufAhmedEE <110684819+YoussufAhmedEE@users.noreply.github.com>
2024-12-19 20:05:45 +0200

    add UserId to the request

--- src/middlewares/authentication.middlewares.js
+++ src/middlewares/authentication.middlewares.js
@@ -4,13 +4,14 @@ const { UnauthorizedError } = require("../exceptions/httpExceptions");
 const AuthMiddleware = (req, res, next) => {
   try {
     const token = req.cookies.jwt;
+    
     if (token) {
       const decoded = jwt.decode(token);
-
-  
+      
       // User Role
       if (decoded && decoded.userId) {
         const userId = decoded.userId;
+        req.userId = userId;
         req.userId = userId;
       } else {
         throw new UnauthorizedError("Invalid JWT");
