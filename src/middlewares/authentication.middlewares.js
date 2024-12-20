06c8e13c8c0d2fbd4b0a58b2e225618de46fc65c
YoussufAhmedEE <110684819+YoussufAhmedEE@users.noreply.github.com>
2024-12-20 23:10:50 +0200

    Add Send Message endpoint

--- src/middlewares/authentication.middlewares.js
+++ src/middlewares/authentication.middlewares.js
@@ -1,17 +1,16 @@
 const jwt = require("jsonwebtoken");
 const { UnauthorizedError } = require("../exceptions/httpExceptions");
-
+const {Conversation}=require('../models/conversation.model')
 const AuthMiddleware = (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-    
+    console.log(token)
     if (token) {
       const decoded = jwt.decode(token);
       
       // User Role
       if (decoded && decoded.userId) {
         const userId = decoded.userId;
-        req.userId = userId;
         req.userId = userId;
       } else {
         throw new UnauthorizedError("Invalid JWT");
@@ -27,6 +26,26 @@ const AuthMiddleware = (req, res, next) => {
 };
 
 
+const AuthConcersationIdMiddleware =async(req, res, next) =>{
+  try {
+    const conversationId= req.body.conversationId
+    const userId=req.userId 
 
+    const conversation = await Conversation.findOne({
+      where: {
+        conversationId,
+        userId,
+      },
+    });
 
-module.exports = { AuthMiddleware };
+    if (!conversation) {
+      throw new UnauthorizedError("Unauthorized conversation");
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+} 
+
+module.exports = { AuthMiddleware, AuthConcersationIdMiddleware };
