f2f11d126d7f648083c1bf6298e44580f78d8227
YoussufAhmedEE <110684819+YoussufAhmedEE@users.noreply.github.com>
2025-02-13 21:55:06 +0200

    Update the Authentication and the type of users

--- src/middlewares/authentication.middlewares.js
+++ src/middlewares/authentication.middlewares.js
@@ -1,13 +1,14 @@
 const jwt = require("jsonwebtoken");
 const { UnauthorizedError } = require("../exceptions/httpExceptions");
 const {Conversation}=require('../models/conversation.model')
+
 const AuthMiddleware = (req, res, next) => {
   try {
     const token = req.cookies.jwt;
   
     if (token) {
-      const decoded = jwt.decode(token);
-      
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
       // User Role
       if (decoded && decoded.userId) {
         const userId = decoded.userId;
@@ -24,7 +25,6 @@ const AuthMiddleware = (req, res, next) => {
     next(error);
   }
 };
-
 
 const AuthConversationIdMiddleware =async(req, res, next) =>{
   try {
@@ -48,4 +48,33 @@ const AuthConversationIdMiddleware =async(req, res, next) =>{
   }
 } 
 
-module.exports = { AuthMiddleware, AuthConversationIdMiddleware };
+// Middleware to check if the user has a specific role
+const authorizeRole = (requiredRole) => (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      throw new UnauthorizedError("Invalid JWT");
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    if (!decoded ||!decoded.roles || !decoded.roles.includes(requiredRole)) {
+      throw new UnauthorizedError("Unauthorized User");
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Middleware for Admin
+const isAdmin = authorizeRole("Admin");
+
+// Middleware for ShopOwner
+const isShopOwner = authorizeRole("ShopOwner");
+
+
+
+
+module.exports = { AuthMiddleware, AuthConversationIdMiddleware,isAdmin, isShopOwner  };
