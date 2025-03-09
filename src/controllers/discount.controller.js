2c711a27760014ef595a49c149defbb58362562b
YoussufAhmedEE <110684819+YoussufAhmedEE@users.noreply.github.com>
2025-03-09 16:56:45 +0200

    Disounct(model-return-big updates in logic)

--- src/controllers/discount.controller.js
+++ src/controllers/discount.controller.js
@@ -1,62 +1,77 @@
 const { Model } = require("sequelize")
 const{discountProductServices,discountCategoryServices,
-    removeProductDiscountService,removeCategoryDiscountService}=require('../services/discount.services')
+    removeProductDiscountService,removeCategoryDiscountService,getDiscountsService}=require('../services/discount.services');
+const { Product } = require("../models/product.models");
 
 const discountProductController=async (req, res)=>{
    try{
-    const {productId, percentage}= req.body;
-    const result = await discountProductServices(productId,percentage);
+    const {productId, percentage, startDate,endDate}= req.body;
+    const adminId= req.userId;
+
+    const result = await discountProductServices(productId, percentage, startDate,endDate,adminId);
     
-    res.status(200).json({ result });
+    res.status(200).json({message: result.message});
 
    }catch(err){
-    res.status(400).json({ error: err.message });
-   }
-        
+    res.status(400).json({ message: err.message });
+   }     
 }
 
 const discountCategoryController=async (req, res)=>{
     try{
-        const {categoryName, percentage}= req.body;
+        const {categoryId, percentage, startDate,endDate}= req.body;
+        const adminId= req.userId;
 
-        const result = await discountCategoryServices(categoryName,percentage);
+        const result = await discountCategoryServices(categoryId, percentage, startDate,endDate,adminId);
 
-        res.status(200).json({ result });
+        res.status(200).json({ message:result.message, products:result.updatedProducts });
     }catch(err){
-        res.status(400).json({ error: err.message });
+        res.status(400).json({ message: err.message });
     }
 }
 
 
-const removeProductDiscountController=async (req,res)=>{
-   try{
 
-    const {productId}= req.body
-
-    const result= await removeProductDiscountService(productId);
-
-    res.status(200).json(result);
-   }catch(err){
-    res.status(400).json({message: err.message})
-    
-   }
-
+const  getDiscountsController = async (req, res)=> {
+    try {
+        const { status } = req.query; // ممكن يكون "valid" أو غير موجود
+        const data = await getDiscountsService(status);
+        
+        res.json({ message: "the discounts returned successfullly", data  });
+    } catch (error) {
+        res.status(500).json({message: error.message });
+    }
 }
+
+const removeProductDiscountController=async (req,res)=>{
+    try{
+        const {productId, percentage}= req.body;
+    
+        const result = await removeProductDiscountService(productId);
+        
+        res.status(200).json({message:result.message, product:result.product});
+    
+       }catch(err){
+        res.status(400).json({ message: err.message });
+       }   
+}
+
 const removeCategotyDiscountController=async (req,res)=>{
     try{
 
-        const {categoryName}= req.body
+        const {categoryId}= req.body
     
-        const result= await removeCategoryDiscountService(categoryName);
+        const result= await removeCategoryDiscountService(categoryId);
     
-        res.status(200).json(result);
+        res.status(200).json({message:result.message, products:result.updatedProducts});
+    
        }catch(err){
-        res.status(400).json({message: err.message})
-        
+        res.status(400).json({ message: err.message });
        }
     
 }
 module.exports={discountProductController,
                 discountCategoryController,
                 removeProductDiscountController,
-                removeCategotyDiscountController}
+                removeCategotyDiscountController,
+                getDiscountsController}
---
+++ src/models/discounts.model.js
@@ -0,0 +1,113 @@
+
+const { DataTypes } = require('sequelize');
+const { db } = require('../database');
+const {User}=require('./user.models')
+const {Product}=require('./product.models');
+const { Category } = require('./category.models');
+
+const DiscountLogs = db.define('DiscountLogs', {
+    logId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+    adminId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { 
+        model: User,
+        key: 'userId' },
+    },
+    time: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+    },
+    process: {
+    type: DataTypes.ENUM('Terminate', 'Create'),
+    allowNull: false,
+    },
+});
+
+const DiscountOnProducts = db.define('DiscountOnProducts', {
+    discountId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+    productId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+        model: Product,
+        key: 'productId',},
+    },
+    percentage: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    status: {
+    type: DataTypes.ENUM('valid', 'expired'),
+    defaultValue: 'valid',
+    },
+    startDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    },
+    endDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    },
+    logId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+        model: DiscountLogs,
+        key: 'logId',
+    },
+    },
+});
+
+
+const DiscountOnCategories = db.define('DiscountOnCategories', {
+    discountId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+    categoryId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+        model: Category,
+        key: 'categoryId',},
+    },
+    percentage: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    status: {
+    type: DataTypes.ENUM('valid', 'expired'),
+    defaultValue: 'valid',
+    },
+    startDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    },
+    endDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    },
+    logId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+        model: DiscountLogs,
+        key: 'logId',
+    },
+    },
+});
+
+
+
+module.exports={DiscountOnCategories,DiscountOnProducts,DiscountLogs};
