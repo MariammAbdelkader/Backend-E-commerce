c2e6b5e49f1a0f9a0a50016dd1c8c0e3ebf1527d
Mohameedshawky <mohamedshawkymohamed122@gmail.com>
2025-04-27 13:48:55 +0300

    - Fixing a bug in .env to integrate with ai - New endpoint for chatbot integration isolated from the previous product one

--- src/controllers/chatbot.controller.js
+++ src/controllers/chatbot.controller.js
@@ -1,5 +1,5 @@
 const { Conversation } = require('../models/conversation.model');
-const {startConversationservices,sendMessageservices} =require('../services/chatbot.serveices')
+const {startConversationservices,sendMessageservices,getProductsByCategoryServices,AllCategoriesAsListServices, getAllCategoriesAsListServices} =require('../services/chatbot.services')
 
 const startConversation =async (req,res)=>{
     try {
@@ -25,4 +25,37 @@ const sendMessage= async(req,res)=>{
     res.status(200).json({ reply:response.message, conversationId:response.conversationId});
 }
 
-module.exports={startConversation, sendMessage}
+const getProductsByCategory = async (req, res) => {
+    try {
+        const { category } = req.query;
+        
+        if (!category) {
+            return res.status(400).json({ message: "Category is required in query parameters." });
+        }
+
+        const products = await getProductsByCategoryServices(category);
+        return res.status(200).json({ products }); 
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Failed to fetch products by category." });
+    }
+};
+
+const getAllCatigoriesAsListController= async (req,res)=>{
+    try {
+
+        const categories = await getAllCategoriesAsListServices()
+
+        res.status(200).json({
+            message: "All Categories returned sccessfully",
+            categories,
+        });
+
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+
+}
+
+module.exports={startConversation, sendMessage, getProductsByCategory ,getAllCatigoriesAsListController}
