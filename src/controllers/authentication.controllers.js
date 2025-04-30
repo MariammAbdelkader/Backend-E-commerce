fa33a390f26465730e149dc7735216b38ac480ef
Mohameedshawky <mohamedshawkymohamed122@gmail.com>
2025-04-30 20:15:56 +0300

    - Fix a Bug - change some error message

--- src/controllers/authentication.controllers.js
+++ src/controllers/authentication.controllers.js
@@ -9,7 +9,7 @@ const signUp =async (req,res)=>{
     try {
         const signUpData = req.body;
         const response = await signUpService(signUpData);
-        res.status(200).cookie('jwt', response.token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }).json({ message: "User created successfully" });
+        res.status(200).cookie('jwt', response.token, { httpOnly: false, maxAge: 24 * 60 * 60 * 1000 }).json({ message: "User created successfully" });
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
@@ -20,7 +20,7 @@ const login =async (req , res) => {
     try {
         const {email , password} =  req.body ; 
         const loginResponse = await loginService(email , password);
-        res.status(200).cookie('jwt' , loginResponse.token , {httpOnly:true , maxAge : 24 * 60 * 60 * 1000}).json({message : "Logged in Succesfully" , data:loginResponse.data});
+        res.status(200).cookie('jwt' , loginResponse.token , {httpOnly:false , sameSite: 'None',  secure: false ,  maxAge : 24 * 60 * 60 * 1000}).json({message : "Logged in Succesfully" , data:loginResponse.data});
         
     } catch (err) {
         res.status(400).json({ error : err.message });
