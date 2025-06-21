2acf3e4aca14796cd7b70829ab4102684485737f
Mohameedshawky <mohamedshawkymohamed122@gmail.com>
2025-06-21 18:26:35 +0300

    - Update CSV path to be under products

--- src/controllers/csv.controllers.js
+++ src/controllers/csv.controllers.js
@@ -2,17 +2,21 @@
 
 const { uploadCsvService } = require("../services/csv.services");
 
-const uploadCsv =async (req , res) => {
-    try {
-        const filePath =  req.file.path ; 
-        const response = await uploadCsvService(filePath);
-        res.status(200).json({message : "uploaded succesfully",response });
-        
-        
-    } catch (err) {
-        res.status(400).json({ error : err.message });
-     }
+const uploadCsv = async (req, res) => {
+  try {
+    const filePath = req.file.path;
+    await uploadCsvService(filePath);
 
-}
+    res.status(200).json({
+      success: true,
+      message: "CSV uploaded successfully!",
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      error: err.message || "Failed to upload CSV.",
+    });
+  }
+};
 
 module.exports={uploadCsv}
